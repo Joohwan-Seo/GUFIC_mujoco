b9f803c8778ddc2401ee9d52295f3d7f6cca6344
Joohwan <joohwan_seo@berkeley.edu>
2025-01-29 20:19:36 -0800

    adding git ignore and working version of gufic

--- gufic_env/env_gufic.py
+++ gufic_env/env_gufic.py
@@ -19,7 +19,7 @@ import matplotlib.pyplot as plt
 
 
 class RobotEnv:
-    def __init__(self, robot_name = 'indy7', env_type = 'square_PIH', max_time = 10, show_viewer = False, 
+    def __init__(self, robot_name = 'indy7', env_type = 'square_PIH', max_time = 6, show_viewer = False, 
                  obs_type = 'pos', hole_ori = 'default', testing = False, reward_version = None, window_size = 1, 
                  use_ext_force = False, act_type = 'default', mixed_obs = False, hole_angle = 0.0, in_dist = True, fix_camera = False):
         
@@ -52,7 +52,7 @@ class RobotEnv:
         # (by the definition of the rotational matrix.)
 
         # self.pd = np.array([0.50, 0.05, 0.13])
-        self.pd = np.array([0.60, 0.05, 0.40])
+        self.pd = np.array([0.45, 0.05, 0.15])
         self.Rd = np.array([[0, 1, 0],
                             [1, 0, 0],
                             [0, 0, -1]])
@@ -61,6 +61,8 @@ class RobotEnv:
         self.R_plate = np.array([[0, 1, 0],
                             [1, 0, 0],
                             [0, 0, -1]])
+        
+        self.z_init_offset = -0.3
 
         self.show_viewer = show_viewer
         self.load_xml()
@@ -78,7 +80,11 @@ class RobotEnv:
 
         ## For the GIC part
         self.Kp = np.eye(3) * np.array([1000, 1000, 300])
+        # self.Kp = np.eye(3) * np.array([1000, 1000, 50])
+
         self.KR = np.eye(3) * np.array([750, 750, 750])
+
+        self.Kd = np.eye(6) * np.array([200, 200, 200, 200, 200, 200])
 
         ## For the force tracking
         self.e_force_prev = np.zeros((6,1))
@@ -89,7 +95,7 @@ class RobotEnv:
         # self.kd_force = 0
         # self.ki_force = 0.6
 
-        self.kp_force = 0.01
+        self.kp_force = 0.005
         self.kd_force = 0
         self.ki_force = 0.6
 
@@ -108,12 +114,14 @@ class RobotEnv:
         self.T_f = 0.5 * self.x_tf**2
         self.T_i = 0.5 * self.x_ti**2
 
+        self.d_max = 0.02
+        self.eR_norm_max = 0.05
+
         ####### Dummy for the printing
         self.Ff_list = []
         self.Vb_list = []
         self.Ff_activation = []
-
-
+        self.rho_list = []
 
     def load_xml(self):
         # dir = "/home/joohwan/deeprl/research/GIC_Learning_public/"
@@ -156,7 +164,7 @@ class RobotEnv:
             Ry = np.array([[np.cos(rand_rpy[1]), 0, np.sin(rand_rpy[1])], [0, 1, 0], [-np.sin(rand_rpy[1]), 0, np.cos(rand_rpy[1])]])
             Rz = np.array([[np.cos(rand_rpy[2]), -np.sin(rand_rpy[2]), 0], [np.sin(rand_rpy[2]), np.cos(rand_rpy[2]), 0], [0, 0, 1]])
 
-            p_init = self.pd.reshape((-1,1)) + self.Rd @ np.array([rand_xy[0], rand_xy[1], -0.2]).reshape(-1,1)
+            p_init = self.pd.reshape((-1,1)) + self.Rd @ np.array([rand_xy[0], rand_xy[1], self.z_init_offset]).reshape(-1,1)
             R_init = self.Rd @ Rz @ Ry @ Rx
 
             p_init = p_init.reshape((-1,))
@@ -168,7 +176,7 @@ class RobotEnv:
             Ry = np.array([[np.cos(rand_rpy[1]), 0, np.sin(rand_rpy[1])], [0, 1, 0], [-np.sin(rand_rpy[1]), 0, np.cos(rand_rpy[1])]])
             Rz = np.array([[np.cos(rand_rpy[2]), -np.sin(rand_rpy[2]), 0], [np.sin(rand_rpy[2]), np.cos(rand_rpy[2]), 0], [0, 0, 1]])
 
-            p_init = self.pd.reshape((-1,1)) + self.Rd @ np.array([rand_xy[0], rand_xy[1], -0.2]).reshape(-1,1)
+            p_init = self.pd.reshape((-1,1)) + self.Rd @ np.array([rand_xy[0], rand_xy[1], self.z_init_offset]).reshape(-1,1)
             R_init = self.Rd @ Rz @ Ry @ Rx
 
             p_init = p_init.reshape((-1,))
@@ -395,7 +403,44 @@ class RobotEnv:
 
         F_f = - self.kp_force * e_force - self.kd_force * de_force - self.ki_force * int_force + Fd
 
-        # F_f = F_f.clip(-20,20)
+        #2.5 Apply shaping function to the force control input
+        f_f = F_f[:3].reshape((-1,))
+        m_f = F_f[3:].reshape((-1,))
+
+        eg = self.get_eg()
+
+        ep = eg[:3,0]
+        eR = eg[3:,0]
+
+        rho_p = np.zeros((3,))
+        rho_R = np.zeros((3,))
+
+        if self.time_step % 100 == 0:
+            print(ep[2], f_f[2])
+            print(ep @ f_f)
+
+        if ep @ f_f >= 0:
+            rho_p[:3] = 1
+        elif ep @ f_f < 0:
+            for i in range(3):
+                if ep[i] <= 0 and np.abs(ep[i]) <= self.d_max:
+                    rho_p[i] = 0.5 * (1 + np.cos(np.pi * ep[i] / self.d_max))
+        else:
+            rho_p[:3] = 0
+
+        eR_norm = np.linalg.norm(eR)
+        if eR @ m_f >= 0:
+            rho_R[:3] = 1
+        elif eR @ m_f < 0:
+            if eR_norm <= self.eR_norm_max:
+                rho_R[:3] = 0.5 * (1 + np.cos(np.pi * eR_norm / self.eR_norm_max))
+        else:
+            rho_R[:3] = 0
+
+        rho = np.block([rho_p, rho_R]).reshape((-1,1))
+
+        # ensure element-wise multiplication
+        F_f = F_f * rho
 
         self.e_force_prev = e_force
         self.int_force_prev = int_force
@@ -455,7 +500,8 @@ class RobotEnv:
         Vd_star_mod = (gamma_i + alpha_i * (1 - gamma_i)) * Vd_star
         ev_mod = Vb - Vd_star_mod
 
-        Kd = np.sqrt(np.block([[Kp, np.zeros((3,3))],[np.zeros((3,3)), KR]])) * 10
+        # Kd = np.sqrt(np.block([[Kp, np.zeros((3,3))],[np.zeros((3,3)), KR]])) * 10
+        Kd = self.Kd
 
         dx_ti = (beta_i / self.x_ti) * (gamma_i * inner_product_i + (ev_mod.T @ Kd @ ev_mod)[0,0]) \
                 + (alpha_i / self.x_ti) * (1 - gamma_i) * inner_product_i
@@ -479,6 +525,7 @@ class RobotEnv:
         self.Ff_list.append(F_f)
         self.Vb_list.append(Vb)
         self.Ff_activation.append(gamma_f + alpha_f*(1 - gamma_f))
+        self.rho_list.append(rho)
 
         return tau_cmd.reshape((-1,))
     
@@ -492,7 +539,7 @@ if __name__ == "__main__":
     show_viewer = True
     angle = 0
     angle_rad = angle / 180 * np.pi
-    RE = RobotEnv(robot_name, env_type, show_viewer = True, obs_type = 'pos', window_size = 1, hole_ori = 'default', 
+    RE = RobotEnv(robot_name, env_type, show_viewer = show_viewer, obs_type = 'pos', window_size = 1, hole_ori = 'default', 
                   use_ext_force = False, testing = True, act_type = 'minimal', reward_version = 'force_penalty',
                   hole_angle = angle_rad, fix_camera = False)
     p_list, R_list, x_tf_list, x_ti_list, Fe_list, Fd_list = RE.run()
@@ -500,6 +547,7 @@ if __name__ == "__main__":
     Ff_list = RE.Ff_list
     Vb_list = RE.Vb_list
     Ff_activation = RE.Ff_activation
+    rho_list = RE.rho_list
 
     print('Done')
 
@@ -513,6 +561,7 @@ if __name__ == "__main__":
     Ff_arr = np.asarray(Ff_list)
     Vb_arr = np.asarray(Vb_list)
     Ff_activation_arr = np.asarray(Ff_activation)
+    rho_arr = np.asarray(rho_list)
 
     # Perform the inner_product_f value
     inner_product_f_arr = np.zeros((len(Vb_arr),1))
@@ -523,22 +572,35 @@ if __name__ == "__main__":
     plt.figure(1)
     plt.plot(Fe_arr[:,2])
     plt.plot(-Fd_arr[:,2])
+    plt.ylabel('Force z direction')
+    plt.xlabel('Time Step')
 
     plt.figure(2)
     plt.subplot(211)
     plt.plot(x_tf_arr)
+    plt.ylabel('Force Tank State')
     plt.subplot(212)
     plt.plot(x_ti_arr)
+    plt.ylabel('Impedance Tank State')
+    plt.xlabel('Time Step')
 
     # plot tank values T_f = 0.5 * x_tf^2, T_i = 0.5 * x_ti^2
     plt.figure(3)
     plt.subplot(211)
     plt.plot(0.5 * x_tf_arr**2)
+    plt.ylabel('Force Tank Level')
     plt.subplot(212)
     plt.plot(0.5 * x_ti_arr**2)
+    plt.ylabel('Impedance Tank Level')
+    plt.xlabel('Time Step')
 
     plt.figure(4)
     plt.plot(Ff_activation_arr)
+    plt.ylabel('Activation of Ff')
+
+    plt.figure(5)
+    plt.plot(rho_arr[:,2])
+    plt.ylabel('Rho Value')
 
     plt.show()
 
